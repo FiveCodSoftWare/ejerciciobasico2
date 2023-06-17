2eac8fa9003b739ad04297f21fc1bb8745bff883
Antonio Aburto Cortez <aaburto583@gmail.com>
2023-06-17 18:51:58 -0500

    feat: convertir pies a metros

---
+++ ejerciciobasico2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciobasico2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double metros, pies;
+
+            Console.WriteLine("Ingrese pies: ");
+            pies =Convert.ToInt32(Console.ReadLine());
+
+            metros = pies / 3.2808399;
+            Console.WriteLine("Pies a metros: " + metros);
+            Console.ReadKey();
+
+        }
+    }
+}
